6c69fe7b9c4d1dd305f0b20966f6588d4d0f58d9
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-09 15:13:19 +0300

    del console.log

--- src/Components/EditableSpan.tsx
+++ src/Components/EditableSpan.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useCallback, useState} from "react";
 import {Input} from "@mui/material";
 
 
@@ -10,16 +10,15 @@ type EditableSpanType = {
 }
 
 export const EditableSpan = React.memo((props: EditableSpanType) => {
-  console.log('EditableSpan called')
   let [editMode, setEditMode] = useState(false)
   let [inputValue, setInputValue] = useState(props.title)
   const onClickDoubleHandler = () => {
     setEditMode(!editMode)
   }
-  const changeTaskValue = (e: ChangeEvent<HTMLInputElement>) => {
+  const changeTaskValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.currentTarget.value)
     props.callBack(e.currentTarget.value)
-  }
+  },[props.callBack])
   return (
     <>
       {!editMode ? <span style={{marginRight: '10px'}} onDoubleClick={onClickDoubleHandler}>{props.title}</span> : <Input onChange={changeTaskValue} value={inputValue} onBlur={onClickDoubleHandler} autoFocus type="text"/>}
