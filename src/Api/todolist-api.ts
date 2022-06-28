54c258e9f1f7ac8b22a98a6e93e33f276d76a742
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-06-28 22:31:52 +0300

    add updateTask request

--- src/Api/todolist-api.ts
+++ src/Api/todolist-api.ts
@@ -26,7 +26,10 @@ export const todolistAPI = {
     return instance.get<GetTasksResponse>(`todo-lists/${todolistId}/tasks`)
   },
   createTask(todolistId: string, title: string){
-    return instance.post(`todo-lists/${todolistId}/tasks`,{title})
+    return instance.post<CreateTaskResponse>(`todo-lists/${todolistId}/tasks`,{title})
+  },
+  updateTask(todolistId:string, taskId: string, model: UpdateTaskModelType){
+    return instance.put(`todo-lists/${todolistId}/tasks/${taskId}`, model)
   }
 }
 
@@ -71,6 +74,29 @@ export type CreateTaskResponse = {
   },
   totalCount: number,
   error: string | null
+}
+
+
+export enum TaskStatuses {
+  New = 0,
+  InProgress = 1,
+  Completed = 2,
+  Draft = 3
+}
+export enum TaskPriorities {
+  Low = 0,
+  Middle = 1,
+  Hi = 2,
+  Urgently = 3,
+  Later = 4
+}
+export type UpdateTaskModelType = {
+  title: string
+  description: string
+  status: TaskStatuses
+  priority: TaskPriorities
+  startDate: string
+  deadline: string
 }
 
 
