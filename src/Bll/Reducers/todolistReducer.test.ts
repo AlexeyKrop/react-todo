baeb978b4fc5617aa58376561dc5c62b13fa4f8b
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-24 17:26:08 +0300

    check add todo test

--- src/Bll/Reducers/todolistReducer.test.ts
+++ src/Bll/Reducers/todolistReducer.test.ts
@@ -1,31 +1,31 @@
-import {TodolistDomainType} from "./todolistReducer";
+import {addTodolistAC, TodolistDomainType, todolistReducer} from "./todolistReducer";
 import {v1} from "uuid";
 
-// let startState: Array<TodolistDomainType> = []
-// let todolistID_1: string
-// let todolistID_2: string
-// beforeEach(() => {
-//   todolistID_1 = v1()
-//   todolistID_2 = v1()
-//   startState = [
-//     {
-//       id: todolistID_1,
-//       title: 'What to Learn',
-//       addedDate: 'string',
-//       order: -1,
-//       filter: 'all'
-//     },
-//     {
-//       id: todolistID_1,
-//       title: 'What to Learn',
-//       addedDate: 'string',
-//       order: -1,
-//       filter: 'all'
-//     }
-//   ]
-// })
-// test('check add Todolist', () => {
-//   let endState = todolistReducer(startState , addTodolistAC('new todo'))
-//   expect(endState.length).toBe(3)
-//   expect(endState[0].title).toBe('new todo')
-// })
+let startState: Array<TodolistDomainType> = []
+let todolistID_1: string
+let todolistID_2: string
+beforeEach(() => {
+  todolistID_1 = v1()
+  todolistID_2 = v1()
+  startState = [
+    {
+      id: todolistID_1,
+      title: 'What to Learn',
+      addedDate: 'string',
+      order: -1,
+      filter: 'all',
+      entityStatus: "idle"
+    },
+  ]
+})
+test('check add Todolist', () => {
+  let newTodo = {
+    id: todolistID_2,
+    title: 'new todo',
+    addedDate: 'string',
+    order: -2
+  }
+  let endState = todolistReducer(startState , addTodolistAC(newTodo))
+  expect(endState.length).toBe(2)
+  expect(endState[0].title).toBe('new todo')
+})
