b14846fd6bb8bd6de025cad2f1cf1a0c60dadf88
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-07-28 20:53:13 +0300

    fix type

--- src/Components/Tasks.tsx
+++ src/Components/Tasks.tsx
@@ -2,7 +2,7 @@ import React, {ChangeEvent, useCallback} from 'react';
 import {EditableSpan} from "./EditableSpan";
 
 
-import {TaskStatuses} from "../Api/todolist-api";
+import {TaskStatuses, TaskType} from "../Api/todolist-api";
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from "@mui/material/IconButton/IconButton";
 import {Delete} from '@mui/icons-material';
@@ -14,26 +14,26 @@ type TaskPropsType = {
   changeTaskStatus: (todoListId: string, taskId: string, status: TaskStatuses) => void
   onChangeInputValue: (todoListId: string, taskId: string, changeInputValue: string) => void
   todoListId: string
-  task:  any
+  task: TaskType & {isDisabled?: boolean}
   entityStatus: RequestStatusType
 }
 const Tasks = React.memo((props: TaskPropsType) => {
-  const onClickHandler = useCallback(() => props.removeTask(props.todoListId, props.task.id),[props])
+  const onClickHandler = useCallback(() => props.removeTask(props.todoListId, props.task.id), [props])
   const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     props.changeTaskStatus(props.todoListId, props.task.id, e.currentTarget.checked ? TaskStatuses.Completed : TaskStatuses.New);
-  },[ props])
+  }, [props])
 
   const onChangeInputValue = useCallback((inputValue: string) => {
     props.onChangeInputValue(props.todoListId, props.task.id, inputValue)
-  },[props])
+  }, [props])
 
   return <div className={props.task.status === TaskStatuses.Completed ? "is-done" : ""}>
     <Checkbox onChange={onChangeHandler} checked={props.task.status === TaskStatuses.Completed}/>
-  <EditableSpan disabled={props.entityStatus === "loading"} callBack={onChangeInputValue} title={props.task.title}/>
-  <IconButton disabled={props.task.isDisabled}  onClick={onClickHandler} aria-label="delete" size="large">
-    <Delete/>
-  </IconButton>
-</div>
+    <EditableSpan disabled={props.entityStatus === "loading"} callBack={onChangeInputValue} title={props.task.title}/>
+    <IconButton disabled={props.task.isDisabled} onClick={onClickHandler} aria-label="delete" size="large">
+      <Delete/>
+    </IconButton>
+  </div>
 });
 
 export default Tasks;
