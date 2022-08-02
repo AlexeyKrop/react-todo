7c5d1b21a89a0f3e2a763fe358efa819324b6625
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-02 22:35:49 +0300

    add isLoad

--- src/Components/Backdrop/Backdrop.tsx
+++ src/Components/Backdrop/Backdrop.tsx
@@ -7,15 +7,13 @@ import {selectApp} from "../../Bll/Reducers/appReducer";
 
 export const SimpleBackdrop = () => {
   const {isLoad} = useAppSelector(selectApp)
-
-  console.log(isLoad)
   return (
     <div>
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
         open={isLoad}
       >
-        <CircularProgress color="inherit" />
+        <CircularProgress />
       </Backdrop>
     </div>
   );
