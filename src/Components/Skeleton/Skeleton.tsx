ef4c5a4692619abeba08e765e1977b1704ca29bd
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-03 09:05:48 +0300

    add style from Skeletons

--- src/Components/Skeleton/Skeleton.tsx
+++ src/Components/Skeleton/Skeleton.tsx
@@ -7,34 +7,22 @@ import Container from '@mui/material/Container/Container';
 
 
 export const Skeletons = () => {
+  // the count of elements? which we want show to display before initialized
+  const data = [1,2,3]
   return (
     <Container fixed>
       <Grid container direction="row"
             alignItems="flex-start">
-        <Grid style={{padding: '20px 0'}}>
-          <Paper style={{padding: '20px', marginRight: '10px'}}>
-            <Stack spacing={1}>
-              <Skeleton variant="text" />
-              <Skeleton variant="rectangular" width={260} height={260}/>
-            </Stack>
-          </Paper>
-        </Grid>
-        <Grid style={{padding: '20px 0'}}>
-          <Paper style={{padding: '20px', marginRight: '10px'}}>
-            <Stack spacing={1}>
-              <Skeleton variant="text" />
-              <Skeleton variant="rectangular" width={260} height={260}/>
-            </Stack>
-          </Paper>
-        </Grid>
-        <Grid style={{padding: '20px 0'}}>
-          <Paper style={{padding: '20px', marginRight: '10px'}}>
-            <Stack spacing={1}>
-              <Skeleton variant="text" />
-              <Skeleton variant="rectangular" width={260} height={260}/>
-            </Stack>
-          </Paper>
-        </Grid>
+        {data.map((el, index) => {
+          return  <Grid style={{padding: '20px 0'}}>
+            <Paper style={{padding: '20px', marginRight: '10px'}}>
+              <Stack spacing={1}>
+                <Skeleton variant="text" />
+                <Skeleton variant="rectangular" width={260} height={260}/>
+              </Stack>
+            </Paper>
+          </Grid>
+        })}
       </Grid>
 
     </Container>
