3793170ad52cfbb5a38a40d4b6b89fb6dd848372
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-04 10:55:08 +0300

    add style from Page

--- src/Components/PageNotFound/PageNotFound.tsx
+++ src/Components/PageNotFound/PageNotFound.tsx
@@ -2,19 +2,13 @@ import s from './PageNotFound.module.css'
 import {useAppDispatch} from "../../Bll/state/hooks";
 import {setAppStatusAC} from "../../Bll/Reducers/appReducer";
 import {useEffect} from "react";
-import KeyboardReturnIcon from '@mui/icons-material/KeyboardReturn';
-import IconButton from '@mui/material/IconButton/IconButton';
-import {useNavigate} from "react-router-dom";
+import {NavLink} from "react-router-dom";
+
 export const Page = () => {
   const dispatch = useAppDispatch()
-  const navigate = useNavigate()
   useEffect(() => {
     dispatch(setAppStatusAC('idle'))
   }, [])
-const onClickHandler = () => {
-
-  navigate('/react-todo')
-}
   return (
     <>
 
@@ -42,9 +36,7 @@ const onClickHandler = () => {
               <h1 className={s.messageTitle}>Page Not Found</h1>
               <p className={s.messageText}>We're sorry, the page you were looking for isn't found here. The link you
                 followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
-              <IconButton onClick={onClickHandler} color="primary" className={s.btn}>
-                <KeyboardReturnIcon /> <span>return to todolist</span>
-              </IconButton>
+              <NavLink to={`/react-todo`}><span>Go Home</span></NavLink>
             </div>
           </div>
 
